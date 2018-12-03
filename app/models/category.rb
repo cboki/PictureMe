6048c0264b914eda75dd705437401d6b68aacdb3
Clement Bouakkaz <clementbouakkaz@gmail.com>
2018-12-03 21:58:09 +0000

    adding relations between tables

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+  has_many :photographer_categories
 end
--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -1,3 +1,8 @@
 class Photographer < ApplicationRecord
   belongs_to :user
+  has_many :appointments
+  has_many :reviews, through: :appointments
+  has_many :photos
+  has_many :photographer_categories
+  has_many :categories, through: :photographer_categories
 end
