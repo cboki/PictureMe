53f2c8286671e668c470e10a0e518cf5abe328f6
Clelia Sapriel <clelia_sapriel@yahoo.fr>
2018-12-04 12:56:09 +0000

    index_creation

--- app/controllers/photographers_controller.rb
+++ app/controllers/photographers_controller.rb
@@ -1,6 +1,15 @@
 class PhotographersController < ApplicationController
   def index
+    @photographers = Photographer.all
   end
-  def show
+
+ def show
+    @Photographer = Photographer.find(params[:id])
+    @review = Review.new
   end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
