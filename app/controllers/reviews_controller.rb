ce949b572d09e0e3fc086bc2cd9577c6e0edb64f
Clelia Sapriel <clelia_sapriel@yahoo.fr>
2018-12-05 12:37:31 +0000

    review form + navbar update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,22 @@
 class ReviewsController < ApplicationController
   def new
-    @appointment = Appointment.find(params[:id])
+    @appointment = Appointment.find(params[:appointment_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
+    @review.appointment = Appointment.find(params[:appointment_id])
+
+    if @review.save
+      redirect_to photographers_path
+    else
+      render 'new'
+    end
   end
 
   private
     def review_params
-      params.require(:review).permit(:content)
+      params.require(:review).permit(:stars, :content)
     end
   end
