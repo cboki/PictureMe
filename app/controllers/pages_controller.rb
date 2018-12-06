e1c25f05daf3482ba944bbc31de2c827ec278dc2
Clelia Sapriel <clelia_sapriel@yahoo.fr>
2018-12-06 18:52:34 +0000

    pour clem with love xxoxo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    ids = Review.where(stars: 5).pluck(:id)
-    @review = Review.find(ids.sample)
   end
 
 end
