86a8b8484efb850d4655e96d9b6393a4dd3155c9
Benjamin Dunisch <bdunisch@googlemail.com>
2018-12-07 12:04:46 +0000

    bugfix

--- app/controllers/photographers_controller.rb
+++ app/controllers/photographers_controller.rb
@@ -5,22 +5,29 @@ class PhotographersController < ApplicationController
 
     if params[:query].present?
       @photographers = PgSearch.multisearch(params[:query])
+      @photographers_with_location = Photographer.where.not(latitude: nil, longitude: nil)
+      @photographers_with_location = @photographers_with_location.where(location: params[:query])
+      @markers = @photographers_with_location.map do |photographer|
+        {
+          lng: photographer.longitude,
+          lat: photographer.latitude
+        }
+end
+      else
+        @photographers = Photographer.all
+        @photographers_with_location = Photographer.where.not(latitude: nil, longitude: nil)
+        @markers = @photographers_with_location.map do |photographer|
+          {
+            lng: photographer.longitude,
+            lat: photographer.latitude
+          }
+        end
+      end
+    end
 
-    else
-    @photographers = Photographer.all
-    @photographers_with_location = Photographer.where.not(latitude: nil, longitude: nil)
-    @markers = @photographers_with_location.map do |photographer|
-      {
-        lng: photographer.longitude,
-        lat: photographer.latitude
-      }
-  end
+
+    def show
+      @photographer = Photographer.find(params[:id])
+      @appointment = Appointment.new
     end
   end
-
-
-  def show
-    @photographer = Photographer.find(params[:id])
-    @appointment = Appointment.new
-  end
-end
